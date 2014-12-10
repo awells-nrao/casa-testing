a2fa4a059852a589c8da6095bebf54e5dacb9e52
Alexis Tejeda <alexis.tejeda@gmail.com>
2014-12-10 16:24:29 -0700

    tunning coverage

--- testc/regression/helper.py
+++ testc/regression/helper.py
@@ -287,7 +287,7 @@ def regressionExecutor(test, custom_argv = None, guide = False, verbosity = 2):
 	py_coverage_tree = [ "%s/lib/python/__casac__" % os.getenv("CASAROOT") ]
 	py_coverage_include = [ "%s/lib/python/task*.py" % os.getenv("CASAROOT") ]
 
-	coverage_instance = coverage.coverage(branch=True, source=py_coverage_tree
+	coverage_instance = coverage.coverage(branch=True, source=py_coverage_tree)
 	coverage_instance.start()
 
 	nose.run(argv = test_argv, addplugins = [psprofile.PSProfile()])
