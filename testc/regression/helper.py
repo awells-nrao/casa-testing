2c87fc14c8267176e9ef109ba0174a7379ec620a
Alexis Tejeda <alexis.tejeda@gmail.com>
2014-12-10 10:43:22 -0700

    updated the jenkins regression script

--- testc/regression/helper.py
+++ testc/regression/helper.py
@@ -263,7 +263,6 @@ class RegressionRunner:
 						"--with-psprofile",
 						"--psprofile-file=%s.json" % test,
 						"--with-coverage",
-						"--cover-branches",
 						"--cover-xml-file=%s.xml" % test,
 						"--cover-package=%s" % "casac"	
 						]
